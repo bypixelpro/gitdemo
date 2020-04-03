cc3fd85df3617e60b3759a8c647b61a5ef0f24c4
David <info@pixelpro.es>
2020-04-03 17:15:53 +0200

    antes de BS

--- src/app/libros/libros.component.ts
+++ src/app/libros/libros.component.ts
@@ -11,20 +11,20 @@ export class LibrosComponent implements OnInit {
 
   libros: any;
   errorHttp: boolean;
-
+  cargando: boolean;
 
   constructor(private http: HttpClient, public Libroclicked: LibroclickedService) {
 
  }
 
-
   ngOnInit(): void {
     this.cargarLista();
+    this.cargando = true;
   }
 
   cargarLista() {
     this.http.get('assets/lista-libros.json').subscribe(
-     (respuesta: Response) => {this.libros = respuesta;},
+     (respuesta: Response) => {this.libros = respuesta; this.cargando = false},
      (respuesta: Response) => {this.errorHttp = true}
     )
 
